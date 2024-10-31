ac6dd0845f0bbffe7a61792bdbcc89a03e58f992
UpUp Wang <upup.ashton.wang@gmail.com>
2024-10-30 20:49:49 -0700

    update finetune setup

--- scripts/get_finetune_setup.sh
+++ scripts/get_finetune_setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 pip install gdown
+pip install wandb
+
+# Enable flash attention
+gh repo clone triton-lang/triton
+cd triton/python
+pip install cmake
+pip install -e .
 
 # Get the gdown path for fine-tune data
 get_input() {
@@ -13,6 +20,6 @@ mkdir -p /workspace/MGFM/data/fine-tune
 
 gdown $gdown_path -O /workspace/MGFM/data/fine-tune/
 
-filename=$(ls /workspace/MGFM/data/fine-tune/ -t *.zip | head -n1)
-unzip -q $filename
-rm $filename
+cd 
+unzip -q /workspace/MGFM/data/fine-tune/GUE.zip -d /workspace/MGFM/data/fine-tune/
+rm /workspace/MGFM/data/fine-tune/GUE.zip 
