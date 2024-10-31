2bd2942b3802b749d001547f05102f29ca59f005
UpUp Wang <upup.ashton.wang@gmail.com>
2024-10-30 18:55:42 -0700

    update download script

--- scripts/get_finetune_data.sh
+++ scripts/get_finetune_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pip install gdown
+
 # Get the gdown path for fine-tune data
 get_input() {
     read -p "$1: " value
@@ -10,3 +12,7 @@ gdown_path=$(get_input "Enter your remote gdown path for finetune dataset")
 mkdir -p /workspace/MGFM/data/fine-tune
 
 gdown $gdown_path -O /workspace/MGFM/data/fine-tune/
+
+filename=$(ls /workspace/MGFM/data/fine-tune/ -t *.zip | head -n1)
+unzip -q $filename
+rm $filename
