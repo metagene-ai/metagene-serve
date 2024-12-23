841f98757796a8b645286fa7141e87da2c187706
UpUp Ashton Wang <upup.ashton.wang@gmail.com>
2024-12-23 00:15:57 -0800

    add tokenizer files

--- serving/scripts/set/task/set_main.sh
+++ serving/scripts/set/task/set_main.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # python 3.10 + cuda 11.8.0
 
+
 # add the following to your .bashrc or running scripts
 #export BNB_CUDA_VERSION=118
 #export CUDA_HOME=$CONDA_PREFIX
@@ -13,21 +14,19 @@ export OMP_NUM_THREADS=1
 conda clean -a -y
 mamba clean -a -y
 
-# cuda, gcc/g++, torch
+# cuda, gcc/g++, and torch
 conda install cuda -c nvidia/label/cuda-11.8.0 -y
 mamba install pytorch=2.4.1 pytorch-cuda=11.8 -c pytorch -c nvidia -y
 
+# litgpt
+pip install 'litgpt[all]'
+pip install optimum
+
 # bitsandbytes
 mamba install bitsandbytes=0.45.0 -c conda-forge --no-pyc -y
-pip install psutil transformers accelerate
 
 # autoawq
 pip install autoawq
-
-# gptq from source to enable cuda extensions (need to be in a GPU env)
-GPTQ_PATH="./serving/quantize/AutoGPTQ"
-git clone https://github.com/PanQiWei/AutoGPTQ "${GPTQ_PATH}"
-cd "${GPTQ_PATH}" && pip install -e . && cd -
 
 # quanto
 pip install optimum-quanto
